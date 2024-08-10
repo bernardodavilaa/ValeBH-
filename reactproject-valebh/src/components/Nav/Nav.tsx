fdb0fcaa78701bcc2aab57cd9e3083715c046c39
Bernardo <bebartholomeu@hotmail.com>
2024-08-10 11:55:16 -0300

    Settando o projeto

---
+++ reactproject-valebh/src/components/Nav/Nav.tsx
@@ -0,0 +1,9 @@
+export function Nav(){
+    return (
+        <nav className="w-full h-20 bg-gray-200 border-gray-400">
+            <p>
+                Schedule<span>Mail</span>
+            </p>
+        </nav>
+    )
+}
